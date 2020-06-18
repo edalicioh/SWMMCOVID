9836ba242c72001bf732dc979294d850cc072723
Edalicio Heinzen <edalicio@outlook.com>
2020-06-18 08:19:48 -0300

    address

--- resources/views/dashboard/pages/address/create.blade.php
+++ resources/views/dashboard/pages/address/create.blade.php
@@ -18,9 +18,89 @@
         @include('dashboard.pages.address.components._fromDistrict')
     </div>
 </div>
+
 @stop
 
 
 @section('js')
-<script></script>
+<script>
+
+    document.getElementById('search-state').addEventListener('click', e => {
+        e.preventDefault()
+        const state = document.getElementById('state_name').value
+
+        if (state) {
+            axios.get(`https://nominatim.openstreetmap.org/search?format=json&limit=1&namedetails=1&state=${state}`)
+                .then(res => {
+                    if (res.data.length){
+                        const conf = confirm("Confirmação do nome: " + res.data[0].display_name );
+
+                        if (conf) {
+                            document.getElementById('state_name').value =res.data[0].namedetails.name
+                            document.getElementById('uf').value = res.data[0].namedetails.short_name
+                            document.getElementById('state_coordinates').value = res.data[0].lat+','+res.data[0].lon
+                        }
+                    } else {
+                        alert('Dados errados')
+                    }
+            })
+        } else {
+            alert('Para a busca o campo nome deve ser preenchido')
+        }
+    })
+
+    document.getElementById('search-city').addEventListener('click', e => {
+        e.preventDefault()
+        const city = document.getElementById('city_name').value
+        const state = document.getElementById('state').options[document.getElementById('state').selectedIndex].innerText
+
+
+        if (city) {
+            axios.get(`https://nominatim.openstreetmap.org/search?format=json&limit=1&namedetails=1&state=${state}&city=${city}`)
+                .then(res => {
+
+                    if (res.data.length){
+                        const conf = confirm("Confirmação do nome: " + res.data[0].display_name );
+
+                        if (conf) {
+                            document.getElementById('city_name').value =res.data[0].namedetails.name
+                            document.getElementById('city_coordinates').value = res.data[0].lat+','+res.data[0].lon
+                        }
+                    } else {
+                        alert('Dados errados')
+                    }
+            })
+        } else {
+            alert('Para a busca o campo nome deve ser preenchido')
+        }
+    })
+
+    document.getElementById('search-district').addEventListener('click', e => {
+        e.preventDefault()
+
+        const district  = document.getElementById('district_name').value
+        const city = document.getElementById('city').options[document.getElementById('city').selectedIndex].innerText
+
+        if (district) {
+            axios.get(`https://nominatim.openstreetmap.org/search?format=json&namedetails=1&q=${district}+${city}`)
+                .then(res => {
+                    if (res.data.length){
+                        const conf = confirm("Confirmação do nome: " + res.data[0].display_name );
+
+                        if (conf) {
+                            document.getElementById('district_name').value =res.data[0].namedetails.name
+                            document.getElementById('district_coordinates').value = res.data[0].lat+','+res.data[0].lon
+                        }
+                    } else {
+                        alert('Dados errados')
+                    }
+
+            })
+        } else {
+            alert('Para a busca o campo nome deve ser preenchido')
+        }
+    })
+
+
+</script>
 @stop
